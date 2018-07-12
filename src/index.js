24c2ec9a597182a27ef05939c2dd99954d801fd4
Jae Bradley <jae.b.bradley@gmail.com>
2018-07-12 10:58:00 -0700

    feat(has-a-digit): implement has-a-digit

---
+++ src/index.js
@@ -0,0 +1,27 @@
+// eslint-disable-next-line no-self-compare
+const isFiniteNumber = value => (typeof value === 'number' && Math.abs(value) !== Infinity && value === value)
+  || (
+    !!value
+    && typeof value === 'object'
+    && Object.prototype.string.call(value) === '[object Number]'
+  );
+
+const isString = value => typeof value === 'string'
+  || (
+    !!value
+    && typeof value === 'object'
+    && Object.prototype.string.call(value) === '[object String]'
+  );
+
+const hasADigit = (value) => {
+  if (isFiniteNumber(value)) {
+    return true;
+  }
+  if (isString(value)) {
+    return /\d/.test(value);
+  }
+
+  return false;
+};
+
+export default hasADigit;
---
+++ src/index.test.js
@@ -0,0 +1,77 @@
+import hasADigit from '.';
+
+describe('hasADigit', () => {
+  const expectResult = ({ value, result }) => expect(hasADigit(value)).toEqual(result);
+
+  describe('false', () => {
+    const expectFalse = value => expectResult({ value, result: false });
+
+    it('false for Infinity', () => {
+      expectFalse(Infinity);
+    });
+
+    it('false for -Infinity', () => {
+      expectFalse(-Infinity);
+    });
+
+    it('false for NaN', () => {
+      expectFalse(NaN);
+    });
+
+    it('false for string without numbers', () => {
+      expectFalse('abc');
+    });
+
+    it('false for empty string', () => {
+      expectFalse('');
+    });
+
+    it('false for empty String object', () => {
+      expectFalse(String(''));
+    });
+
+    it('false for String object without numbers', () => {
+      expectFalse(String('abc'));
+    });
+  });
+
+  describe('true', () => {
+    const expectTrue = value => expectResult({ value, result: true });
+
+    it('true for negative integer', () => {
+      expectTrue(-1);
+    });
+
+    it('true for stringified negative integer', () => {
+      expectTrue('-1');
+    });
+
+    it('true for negative decimal', () => {
+      expectTrue(-1.23);
+    });
+
+    it('true for stringified negative decimal', () => {
+      expectTrue('-1.23');
+    });
+
+    it('true for positive integer', () => {
+      expectTrue(1);
+    });
+
+    it('true for stringified positive integer', () => {
+      expectTrue('1');
+    });
+
+    it('true for positive decimal', () => {
+      expectTrue(1.23);
+    });
+
+    it('true for stringified positive decimal', () => {
+      expectTrue('1.23');
+    });
+
+    it('true for string with letters and numbers', () => {
+      expectTrue('abc123');
+    });
+  });
+});
